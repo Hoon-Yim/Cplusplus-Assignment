0c22f15a964bfbe69c8f7dde6190c1a043963277
Hoon-Yim <yimsh4507@gmail.com>
2020-10-08 16:01:52 -0400

    No problem

--- main.cpp
+++ main.cpp
@@ -1,25 +1,10 @@
 #include <iostream>
 
-class MyClass {
-private:
-    int size;
-    int *element;
-
-public:
-    MyClass(int size) : size(size) {
-        element = new int[size];
-        for(int i = 0; i < size; i++) element[i] = i;
-    }
-    MyClass(const MyClass &copy) {
-        this->size = copy.size;
-        this->element = new int[copy.size];
-        for(int i = 0; i < size; i++) { element[i] = copy.element[i]; }
-    }
-    int at(int loc) { return element[loc]; }
-    ~MyClass() { if(element) delete[] element; }
-};
 int main() {
-    MyClass a(5);
-    MyClass b(a);
-    std::cout << b.at(2) << std::endl;
+    int *ptr = nullptr;
+
+    std::cout << ptr << ' ' << (long long)ptr << std::endl;
+    ptr++;
+    std::cout << ptr << ' ' << (long long)ptr << std::endl;
+    return 0;
 }
